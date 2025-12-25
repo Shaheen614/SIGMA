08b201d781aa56940cdb6b37365026988595a209
mishhtachio <mishel.muhammed@gmail.com>
2025-12-25 14:39:14 +0400

    Update timeseries_dataset.py

--- src/lstm/timeseries_dataset.py
+++ src/lstm/timeseries_dataset.py
@@ -2,10 +2,6 @@ import torch
 from torch.utils.data import Dataset
 
 class TimeSeriesDataset(Dataset):
-    """
-    Converts a 1D time series into:
-    [t1, t2, t3] -> t4
-    """
 
     def __init__(self, series, window_size):
         self.series = torch.tensor(series, dtype=torch.float32)
@@ -16,11 +12,6 @@ class TimeSeriesDataset(Dataset):
         return len(self.series) - self.window_size
 
     def __getitem__(self, idx):
-        """
-        Returns:
-        X -> (window_size, 1)
-        y -> scalar
-        """
         X = self.series[idx:idx + self.window_size]
         y = self.series[idx + self.window_size]
         return X.unsqueeze(-1), y
