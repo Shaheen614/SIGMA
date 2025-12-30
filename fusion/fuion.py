7be70776865ac172b9f6a5c3301bdbbe5a15a0cd
Shaheen614 <Shaheenrajamohamed524@gmail.com>
2025-12-30 22:55:50 +0300

    basic fusion layer + lstm model updated added

---
+++ fusion/fuion.py
@@ -0,0 +1,4 @@
+from src.models.autoencoder_model import Autoencoder
+from src.models.lstm_model import LSTMForecast
+from src.models.rul_model import RULModel
+from src.models.gnn_model import GNNModel
